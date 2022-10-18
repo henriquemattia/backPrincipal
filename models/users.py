25d30b8cf6f04ee18aa869346a2ea0e4972c2bc1
henriquemattia <mattia.henrique@gmail.com>
2022-10-18 18:18:20 -0300

    Reorganizando código

--- models/users.py
+++ models/users.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String, Integer
 class UsersModel(Base):
     __tablename__ = "users"
 
-    user_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     email = Column(String, unique=True)
     password = Column(String)
